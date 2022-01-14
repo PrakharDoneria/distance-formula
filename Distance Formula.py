7509b46faf95968b35f8f31c52d7d8121a847a6e
Prakhar Doneria <92568876+PrakharDoneria@users.noreply.github.com>
2022-01-14 09:16:43 +0530

    Add files via upload

---
+++ Distance Formula.py
@@ -0,0 +1,13 @@
+#Define values
+x1 =int(input("Entre x1:"))
+x2 =int(input("Entre x2:"))
+y1 =int(input("Entre y1:"))
+y2 =int(input("Entre y2:"))
+#This would calculate the above
+x = x2 - x1
+X=x*x
+y = y2 -y1
+Y=y*y
+new=X+Y
+print("The distance between the two points is:", new)
+#Their is a problem in this program this would not calculate under root please help if you know which syntax will make it work
