4ede23e9deffc5b353b1e6743ca5ac2b81074c46
Prakhar Doneria <gamesprotec@gmail.com>
2022-01-19 09:29:16 +0530

    Update Distance Formula.py

--- Distance Formula.py
+++ Distance Formula.py
@@ -1,4 +1,4 @@
-#Define values
+from math import sqrt
 x1 =int(input("Entre x1:"))
 x2 =int(input("Entre x2:"))
 y1 =int(input("Entre y1:"))
@@ -9,7 +9,5 @@ X=x*x
 y = y2 -y1
 Y=y*y
 new=X+Y
-print("Distance between two points will be √",new)
-#Made by ProTec Games
-print("..........................................................")
-print("licence of this software is under ProTec Games")
+distance = sqrt(new)
+print("Distance between two points will be", distance)
